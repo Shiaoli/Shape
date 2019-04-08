29b25216053148ed317a2d36ff6ff95dfa2bc50e
Xiaoli Mao <ntmxl5288@gmail.com>
2019-04-08 17:14:47 -0500

    Now working on add dropdownlist for month and year

---
+++ Shape/Controllers/RecordsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Shape.Models;
+
+namespace Shape.Controllers
+{
+    public class RecordsController : Controller
+    {
+        private RecordsDbContext db = new RecordsDbContext();
+
+        // GET: Records
+        public ActionResult Index()
+        {
+            return View(db.Records.ToList());
+        }
+
+        // GET: Records/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // GET: Records/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Records/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,RecordDate,Weight,Waist")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Records.Add(record);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(record);
+        }
+
+        // GET: Records/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // POST: Records/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,RecordDate,Weight,Waist")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(record).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(record);
+        }
+
+        // GET: Records/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // POST: Records/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Record record = db.Records.Find(id);
+            db.Records.Remove(record);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Shape/Models/Records.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Shape.Models
+{
+    public class Record
+    {
+        public int ID { get; set; }
+        public DateTime RecordDate { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Waist { get; set; }
+    }
+
+    public class RecordsDbContext: DbContext
+    {
+        public DbSet<Record> Records { get; set; } 
+    }
+}
